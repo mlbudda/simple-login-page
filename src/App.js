dd415a6346f07bd1147c3eb3854f2004ed32c865
Ignas Markauskas <ignasmark5@gmail.com>
2023-01-18 20:51:30 +0200

    static version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
+import { useState } from "react";
+
 export default function App() {
-  console.log("ar gaunasi?");
-  return <p>Helloo123456</p>;
+  const [email, setEmail] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("ok");
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="top-section">
+          <button className="login-btn">Log In</button>
+          <p> or </p>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+
+        <form method="post" className="input-section" onSubmit={handleSubmit}>
+          <h1>Welcome Back!</h1>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input type="password" placeholder="Password" />
+          <a href="">Forgot your password?</a>
+
+          <button type="submit" className="form-login-btn" disabled>
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
